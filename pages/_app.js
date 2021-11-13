d4bafaa95d83baa06976c382b12b2eabe4d1dc57
Quinn Cooperstein <quinn.flynn.coop@gmail.com>
2021-11-12 19:58:39 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -48,15 +48,14 @@ function MyApp({ Component, pageProps }) {
         />
         <meta property="og:title" content="Alloew!" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="http://Alloew.com/" />
-        <meta property="og:image" content="https://alloew.com/alloew.png" />
-        <meta property="og:description" content="This is alloew! A place where us and our friends make things for fun!" />
+        <meta property="og:url" content="http://alloew.com/" />
+        <meta property="og:image" content="https://alloew.com/images/alloew.png" />
+        <meta property="og:description" content="This is alloew! A place where us and our friends make things for fun! So far, we have Turboman, and more games are coming! We have also started a blog, and I am working on all the technical stuff behind the scenes. Have Fun!!" />
         <meta name="theme-color" content="#2315bf" />
         <meta name="twitter:card" content="summary_large_image" />
         <link rel="apple-touch-icon" sizes="180x180" href="../apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="../favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="../favicon-16x16.png" />
-        <link rel="manifest" href="../site.webmanifest" />
         <link rel="manifest" href="../site.webmanifest" />
       </Head>
       <nav className="flex flex-wrap w-full bg-gray-900
